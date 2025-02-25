be68e332a6d2f10feb8e4623ca3a372fd40d74e9
Tenebris2 <buiducanh567@gmail.com>
2025-02-25 21:51:23 +0700

    added paymentMethod(RadioGroup), progressBar, amountPicker (hoàn thành bài thực hành bài 3)

--- donation/app/src/main/java/com/example/donation/MainActivity.java
+++ donation/app/src/main/java/com/example/donation/MainActivity.java
@@ -8,10 +8,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button donateButton;
+    private RadioGroup paymentMethod;
+    private ProgressBar progressBar;
+    private NumberPicker amountPicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +26,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
         donateButton = (Button) findViewById(R.id.donateButton);
 
         if (donateButton != null) {
             android.util.Log.v("Donate", "Really got the donate button");
         }
+
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
     }
 
     public void donateButtonPressed(View view) {
